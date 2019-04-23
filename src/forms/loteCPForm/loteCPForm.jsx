0b04f73a8e25b2c5bbb694663acfec1e62256b6d
VITOR EDUARDO CONCESSO DIAS PB003402 <pb003402@pbh.rmi>
2019-04-23 17:04:21 -0300

    lote CP

--- src/forms/loteCPForm/loteCPForm.jsx
+++ src/forms/loteCPForm/loteCPForm.jsx
@@ -18,26 +18,12 @@ class LoteCPForm extends Component{
             valueZonaFiscal: '',
             valueNumQuadra:'',
             valueNumLote:'',
-            logSuggestionsList:[],
-            logTypeList:[]
         }
 
         this.handleSubmit = this.handleSubmit.bind(this)
-        this.onChangeZonaFiscal = this.onChangeZonaFiscal.bind(this)
-        this.onChangeNumQuadra = this.onChangeNumQuadra.bind(this)
-        this.onChangeNumLote = this.onChangeNumLote.bind(this)
+
     }
 
-
-    componentWillMount(){
-        this.props.getLogradouroTypeList()  
-    }    
-    onSelectSuggestion(event){
-        console.log(event.suggestion)
-        this.setState({
-            valueLogradouro: event.suggestion
-        })
-    }
     
     handleSubmit (){        
 
@@ -45,14 +31,15 @@ class LoteCPForm extends Component{
             return null
         }
 
-       axios.get('http://bhmap-hm.pbh.gov.br/v2/api/wfs?version=2.0.0&request=GetFeature&typeName=ide_bhgeo%3ALOTE_APROVADO&outputFormat=application%2Fjson&CQL_FILTER=ZONA_FISCAL%3D%27'+this.state.valueZonaFiscal+'%27%20AND%20QUARTEIRAO%3D%27'+this.state.valueNumQuadra+'%27%20AND%20LOTE%3D%27'+this.state.valueNumLote+'%27')
-            .then(resp=>{                
-                if(resp.data.length > 0){
+       axios.get('http://bhmap-hm.pbh.gov.br/v2/api/wfs?version=2.0.0&request=GetFeature&typeName=ide_bhgeo%3ALOTE_APROVADO&outputFormat=application%2Fjson&CQL_FILTER=ZONA_FISCAL%3D%27'+this.state.valueZonaFiscal+'%27%20AND%20QUARTEIRAO%3D%27'+this.state.valueNumQuadra+'%27%20AND%20LOTE%3D%27'+this.state.valueNumLote.toUpperCase()+'%27')
+            .then(resp=>{ 
+                                  
+                if(resp.data){
 
                     console.log(resp.data)    
     
                 }else{
-                    console.log('Nenhum endereços encontrado!')   
+                    console.log('Nenhum lote encontrado!')   
                 }   
                 })
             .catch(e=>{
@@ -61,43 +48,21 @@ class LoteCPForm extends Component{
 
     }
 
-    onSelectType(event){
-
-        this.setState({valueType: event.value,
-        valueLogradouro:'',
-        logSuggestionsList:[]})
-    }
-
-    onChangeZonaFiscal(event){
-
-        this.setState({valueZonaFiscal: event.target.value})
-    }
-
-    onChangeNumQuadra(event){
-
-        this.setState({valueNumQuadra: event.target.value})
-    }
-
-    onChangeNumLote(event){
-
-        this.setState({valueNumLote: event.target.value})
-    }
-
     render(){        
 
         return(               
             <Form > 
                 <Box direction="row" align="stretch" alignContent="stretch" alignSelf="stretch">                             
                     <Box pad="small" width="150px">
-                        <FormField value={this.state.valueZonaFiscal} onChange={this.onChangeZonaFiscal} 
+                        <FormField value={this.state.valueZonaFiscal} onChange={event => this.setState({valueZonaFiscal: event.target.value})} 
                             placeholder="Nº Zona Fiscal" size="xsmall"  max={99999} type="number" />                                                    
                     </Box>
                     <Box pad="small" width="150px">
                         <FormField value={this.state.valueNumQuadra}
-                                onChange={this.onChangeNumQuadra} placeholder="Nº Quadra" size="xsmall"  max={99999} type="number"/>                                                    
+                                onChange={event => this.setState({valueNumQuadra: event.target.value})} placeholder="Nº Quadra" size="xsmall"  max={99999} type="number"/>                                                    
                     </Box>
                     <Box pad="small" width="150px">
-                        <FormField  size="xsmall"placeholder="Nº Lote" type="number" onChange={this.onChangeNumLote} value={this.state.valueNumLote} max={99999}/>                            
+                        <FormField  size="xsmall"placeholder="Nº Lote" onChange={event => this.setState({valueNumLote: event.target.value})} value={this.state.valueNumLote} max={99999}/>                            
                     </Box>
                     <Box pad="small" width="60px">
                         <Button type="submit" size="small" primary icon={<Search size='small'/>} onClick={this.handleSubmit} onKeyPress={e => {
@@ -112,5 +77,6 @@ class LoteCPForm extends Component{
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ getLogradouroTypeList}, dispatch)
-export default connect( null, mapDispatchToProps)(LoteCPForm)
+//const mapDispatchToProps = dispatch => bindActionCreators({ getLogradouroTypeList}, dispatch)
+//export default connect( null, mapDispatchToProps)(LoteCPForm)
+export default LoteCPForm
